085c1a06c7d9f0847687ad00b7937b208baaf1ff
LLaMeaR <80314008+LLaMeaR@users.noreply.github.com>
2025-08-20 18:38:00 +0900

    Update test_grid_dfs.py

--- python/tests/test_grid_dfs.py
+++ python/tests/test_grid_dfs.py
@@ -1,7 +1,5 @@
 from grid_dfs import count_islands
 
-DIR8 = [(-1,0),(1,0),(0,-1),(0,1), (-1,-1),(-1,1),(1,-1),(1,1)]
-
 def test_islands_basic():
     grid = [
         [1,1,0,0],
@@ -11,26 +9,7 @@ def test_islands_basic():
     ]
     assert count_islands(grid) == 2
 
-def count_islands8(grid):
-    if not grid or not grid[0]:
-        return 0
-    R, C = len(grid), len(grid[0])
-    seen = [[False]*C for _ in range(R)]
-    def dfs(r,c):
-        seen[r][c] = True
-        for dr,dc in DIR8:
-            nr, nc = r+dr, c+dc
-            if 0<=nr<R and 0<=nc<C and grid[nr][nc]==1 and not seen[nr][nc]:
-                dfs(nr,nc)
-    cnt = 0
-    for r in range(R):
-        for c in range(C):
-            if grid[r][c]==1 and not seen[r][c]:
-                cnt += 1
-                dfs(r,c)
-    return cnt
-
-
+# tests/test_grid_dfs.py (추가 예시)
 from grid_dfs import count_islands8
 def test_islands_diagonal_connects():
     grid = [
@@ -38,4 +17,5 @@ def test_islands_diagonal_connects():
         [0,1,0],
         [1,0,1],
     ]
+    # 4방향이면 5개, 8방향이면 1개
     assert count_islands8(grid) == 1
